6615696edbd4b78dbf561fdfcdbb0c02515a0f89
signed-long <mrl@ualberta.ca>
2021-05-11 16:21:52 -0600

    added Docker files

---
+++ web/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+
+if [ "$FLASK_ENV" = "testing" ]
+then
+    echo "Creating the database tables w/ dummy data..."
+    python manage.py load_dummy_db
+    echo "Tables created"
+
+else
+    echo "Creating the database tables..."
+    python manage.py create_db
+    echo "Tables created"
+fi
+
+exec "$@"
