b8adbd8aef72d3ef72304891dcf1f320e85a90cc
signed-long <mrl@ualberta.ca>
2021-05-12 00:34:14 -0600

    changed voulumes path in docker-compose.yml to correct path

--- web/app/bp_auth/routes.py
+++ web/app/bp_auth/routes.py
@@ -1,6 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response
+from app.utils.decorators import creds_required
+from app.utils.helpers import make_json_response
+
 
 bp_auth = Blueprint("bp_auth", __name__)
+
+# Template response json:
+# {
+#   "message": "test_set does not exist",
+#   "details": {},
+#   "description": "The reference set does not exist.",
+#   "http_response": {
+#      "message": "We could not find the resource you requested.",
+#       "code": 404
+#    }
+# }
 
 
 @bp_auth.route("/test", methods=["GET"])
@@ -20,11 +34,12 @@ def register():
 
 
 @bp_auth.route("/login", methods=["POST"])
+@creds_required
 def login():
     '''
 
     '''
-    pass
+    return "login"
 
 
 @bp_auth.route("/logout", methods=["POST"])
