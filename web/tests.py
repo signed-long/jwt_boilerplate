891894b53946f25c28aea09ed6a0f3ca8325dc8d
signed-long <mrl@ualberta.ca>
2021-05-24 14:03:24 -0600

    added tests for refresh route

--- web/tests.py
+++ web/tests.py
@@ -2,7 +2,7 @@ import unittest
 from app import create_app, db
 import json
 from app.models import User
-from app.utils.helpers import decode_jwt_token
+from app.utils.helpers import get_id_from_jwt
 from os import environ
 import time
 
@@ -71,10 +71,10 @@ class FlaskJwtAuthTest(unittest.TestCase):
         # assert these tokens are valid
         with self.app.app_context():
             user_id = User.query.filter_by(email=body["email"]).first().id
-        user_id_at = decode_jwt_token(data["data"]["access_token"],
-                                      environ.get("ACCESS_TOKEN_SECRET"))
-        user_id_rt = decode_jwt_token(data["data"]["refresh_token"],
-                                      environ.get("REFRESH_TOKEN_SECRET"))
+        user_id_at = get_id_from_jwt(data["data"]["access_token"],
+                                     environ.get("ACCESS_TOKEN_SECRET"))
+        user_id_rt = get_id_from_jwt(data["data"]["refresh_token"],
+                                     environ.get("REFRESH_TOKEN_SECRET"))
         self.assertTrue(user_id_at == user_id_rt == user_id)
 
         body = {"email": "NoUser@test.ca", "password": "pass"}
@@ -115,9 +115,46 @@ class FlaskJwtAuthTest(unittest.TestCase):
         res = self.client().get('/test', headers=headers)
         self.assertEqual(res.status_code, 200)
 
-        time.sleep(16)
+        time.sleep(6)
         res = self.client().get('/test', headers=headers)
         self.assertEqual(res.status_code, 401)
+
+    def test_refresh(self):
+        '''
+        '''
+        # register
+        body = {"email": "testUser@test.ca", "password": "pass"}
+        register_res = register_user(self.client, body)
+        self.assertEqual(register_res.status_code, 201)
+
+        # login
+        login_res = login_user(self.client, body)
+        data = json.loads(login_res.data.decode())
+        self.assertEqual(login_res.status_code, 200)
+
+        # get tokens
+        data = json.loads(login_res.data.decode())
+        refresh_token = data["data"]["refresh_token"]
+        access_token = data["data"]["access_token"]
+
+        # expire token
+        time.sleep(6)
+        headers = {"Authorization": "Bearer " + str(access_token)}
+        res = self.client().get('/test', headers=headers)
+        self.assertEqual(res.status_code, 401)
+
+        # get fresh access token
+        body = {"refresh_token": refresh_token}
+        res = self.client().get('/refresh', data=json.dumps(body), content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+
+        data = json.loads(res.data.decode())
+        access_token = data["data"]["access_token"]
+
+        # test new access token
+        headers = {"Authorization": "Bearer " + str(access_token)}
+        res = self.client().get('/test', headers=headers)
+        self.assertEqual(res.status_code, 200)
 
 
 def register_user(client, body):
