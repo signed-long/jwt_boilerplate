a704e3a06f1df0cd6a2cd6660d0a1a2f2ba2fb34
signed-long <mrl@ualberta.ca>
2021-05-24 14:47:42 -0600

    added 405 error handler

--- web/app/bp_errors/handlers.py
+++ web/app/bp_errors/handlers.py
@@ -13,6 +13,15 @@ def error_404(error):
     return make_json_response(status=404, msg=msg)
 
 
+@bp_errors.app_errorhandler(405)
+def error_405(error):
+    """
+    Handels 405 error Method not allowed.
+    """
+    msg = ("ERROR 405: Method not allowed.")
+    return make_json_response(status=405, msg=msg)
+
+
 @bp_errors.app_errorhandler(500)
 def error_500(error):
     """
