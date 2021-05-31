7ca5fbe90818b1cdda0cb9c3d0f8bdcbd879c20f
signed-long <mrl@ualberta.ca>
2021-05-31 13:04:22 -0600

    started using pipenv to support trivy vuln scanning

--- web/app/bp_errors/handlers.py
+++ web/app/bp_errors/handlers.py
@@ -6,26 +6,26 @@ bp_errors = Blueprint('bp_errors', __name__)
 
 @bp_errors.app_errorhandler(404)
 def error_404(error):
-    """
+    '''
     Handels 404 error Page Not Found.
-    """
+    '''
     msg = ("ERROR 404: Page not found.")
     return make_json_response(status=404, msg=msg)
 
 
 @bp_errors.app_errorhandler(405)
 def error_405(error):
-    """
+    '''
     Handels 405 error Method not allowed.
-    """
+    '''
     msg = ("ERROR 405: Method not allowed.")
     return make_json_response(status=405, msg=msg)
 
 
 @bp_errors.app_errorhandler(500)
 def error_500(error):
-    """
+    '''
     Handels 500 error Internal Error.
-    """
+    '''
     msg = ("ERROR 500: An error occured on the server.")
     return make_json_response(status=500, msg=msg)
