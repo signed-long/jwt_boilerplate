0f3e674755cb82e549f1726e1c6c3dee96d60ea0
MichaeLong <mrl@ualberta.ca>
2021-06-15 22:20:00 -0600

    changed length of otp_secret db field

--- auth/app/models.py
+++ auth/app/models.py
@@ -10,22 +10,37 @@ class User(db.Model):
     email = db.Column(db.String(128), unique=True, nullable=False)
     password_hash = db.Column(db.String(60), nullable=False)
     refresh_token = db.Column(db.String(155))
-    otp_secret = db.Column(db.String(16))
+    otp_secret = db.Column(db.String(32))
+
+    def check_pw_hash(self, pt_password):
+        '''
+        Checks a given password against the user's password hash.
+        '''
+        return bcrypt.check_password_hash(self.password_hash, pt_password)
+
+    def set_pw_hash(self, pt_password):
+        '''
+        Sets a user's password hash given a password.
+        '''
+        pw_hash = bcrypt.generate_password_hash(pt_password)
+        self.password_hash = pw_hash.decode("utf-8")
 
     def get_totp_uri(self):
+        '''
+        Returns a otpauth uri that can be shown as a QR code on the client.
+        '''
         return 'otpauth://totp/JWT-Service:{0}?secret={1}&issuer=JWT-Service' \
             .format(self.id, self.otp_secret)
 
     def verify_totp(self, token):
+        '''
+        Verify's a given TOTP with the user's otp_secret.
+        '''
         totp = pyotp.TOTP(self.otp_secret)
         return totp.verify(token)
 
     def set_otp_secret(self):
+        '''
+        Sets the user's otp_secret with a random value.
+        '''
         self.otp_secret = pyotp.random_base32()
-
-    def check_pw_hash(self, given_pw_hash):
-        return bcrypt.check_password_hash(self.password_hash, given_pw_hash)
-
-    def set_pw_hash(self, pt_password):
-        pw_hash = bcrypt.generate_password_hash(pt_password)
-        self.password_hash = pw_hash.decode("utf-8")
